5e99222d9f3e8d01123cbbdda2b1218ab6a64124
Noor <noornabiji4@gmail.com>
2018-10-23 21:56:39 +0530

    product details

---
+++ src/Details.js
@@ -0,0 +1,22 @@
+Products = [
+    {
+        name: "Kalonji Oil",
+        id: "Kalonji Oil",
+        description: "We have been producing 100% pure Kalonji oil for the last so many years by extracting Kalonji oil purely by natural method without taking help of any other chemicals. Our 23 years of rigorous & continued research on Kalonji has given successful results to cure many diseases"
+    },
+    {
+        name: "Zam-Zam-e-Bahar Oil",
+        id: "Zam-Zam-e-Bahar Oil",
+        description: "Specially formulated hair oil to cure hair loss, dandruff, pre-mature graying of hair , revitalizes hair, recovers the damaged hair, headaches, gives sound sleep."
+    },
+    {
+        name: "Rogan-e-Gesudaraaz",
+        id: "Rogan-e-Gesudaraaz",
+        description: "Exclusive treatment for the grey hair due to cold & sinus, gives permanent relief from sinus, nutritive to mind & nourishment to hair"
+    },
+    {
+        name: "Kalonji Herbal Miswak ",
+        id: "Kalonji Herbal Miswak ",
+        description: "It is very much useful for the toothache, gum bleeding, bad odor and the entire tooth related problems."
+    },
+]
